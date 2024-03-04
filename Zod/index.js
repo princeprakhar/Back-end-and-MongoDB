01c6ce3dff499542ad0e44698db044d639c1e19d
princeprakhar <103184248+princeprakhar@users.noreply.github.com>
2024-03-05 00:30:15 +0530

    updated zod

--- Zod/index.js
+++ Zod/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const z = require("zod");
  app = express();
  //defining a schema for validation purpose;
- const validationSchema = z.string();
+ const validationSchema = z.object({
+    "username":z.string().email(),
+    "password": z.string().min(8)
+ });
 app.use(express.json());
 //defining the post method to verify the username and pasword validation.
 app.post('/login',function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-    if(validationSchema.safeParse(username).success && validationSchema.safeParse(password).success)
+    const username = req.body;
+    if(validationSchema.safeParse(username).success )
     {
         res.send({
             msg : "valid input for the login"
